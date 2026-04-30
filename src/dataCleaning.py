a637139354d5e26346e12cc5cd9eafb4ff067c58
Aditya Arpit <a.a.upadhyay@student.utwente.nl>
2026-04-30 22:43:24 +0200

    Some organisational changes like moving albertijn_deals to data folder and creating a dedicated python folder. Also the data cleaning process of albertijn data started.

--- src/dataCleaning.py
+++ src/dataCleaning.py
@@ -5,10 +5,10 @@ print("Pick either or both to webscrap! Jumbo or AH?")
 supermarket = input("")
 if supermarket == "AH":
     all_products = webScrapper.albertijn_data()
-    with open('raw_deals.json', 'w') as f:
+    with open('../raw_deals.json', 'w') as f:
         json.dump(all_products, f)
 elif supermarket == "Jumbo":
     all_j_products = webScrapper.jumbo_data()
-    with open('j_raw_deals.json', 'w') as f:
+    with open('../j_raw_deals.json', 'w') as f:
         json.dump(all_j_products, f)
 
